6976505a6791f0ba05b12ae144c9661f9c18e030
Cengiz Ilerler <cilerler@gmail.com>
2020-01-01 21:12:55 -0500

    initial

---
+++ src/MyApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MyClass;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new Class1();
+            c.DoWork();
+        }
+    }
+}
